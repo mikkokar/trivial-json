bf44c1dfb5c942e8b5a7643332ddf1d68bb160a8
a-mkarjalainen <a-mkarjalainen@expedia.com>
2015-04-20 07:28:15 +0100

    Add rudimentary support for arrays. Add print_to_buffer writer. Add rudimentary test case.

--- json.c
+++ json.c
@@ -15,10 +15,42 @@
 
 static char print_buffer[JS_BUFLEN_SIZE];
 
-static void print_to_stdout(const void *call_arg, const char *output)
+static void print_to_stdout(void *call_arg, const char *output)
 {
     printf("%s", output);
 }
+
+struct print_buffer {
+    char *output;
+    int  max_length;
+    int  next_offset;
+};
+typedef struct print_buffer print_buffer_t;
+
+static void print_to_buffer(void *call_arg, const char *output)
+{
+    int written = 0;
+    int remaining = 0;
+
+    print_buffer_t *context = call_arg;
+
+    remaining = context->max_length - context->next_offset;
+
+    // printf("at offset %03d, wrote: [%s]\n", context->next_offset, output);
+
+    written = snprintf(context->output + context->next_offset, remaining,
+                       "%s", output);
+
+    if (context->next_offset + written > context->max_length) {
+        printf("something fishy going on:\n");
+        printf("next_offset: %d\nwritten: %d\nmax_length: %d\n",
+               context->next_offset, written, context->max_length);
+    }
+    assert(context->next_offset + written <= context->max_length);
+
+    context->next_offset += written;
+}
+
 
 static void indent(js_serialiser_t *s)
 {
@@ -35,25 +67,39 @@ static void link_to_previous(js_serialiser_t *s)
     }
 }
 
-
-void js_document(js_serialiser_t *s)
+static void js_document_init(js_serialiser_t *s)
 {
     memset(s, 0, sizeof(*s));
     s->attribute_count = 0;
     s->print = print_to_stdout;
     s->call_arg = NULL;
-
-    s->print(s->call_arg, "{\n");
-    s->level = 1;
 }
 
-void js_document3(js_serialiser_t *s, 
-                  print_function_t print_function, 
-                  void *call_arg)
+static void js_document_print(js_serialiser_t *s)
 {
-    js_document(s);
+    s->print(s->call_arg, "{\n");
+    s->level = 1;
+    s->in_array = 0;
+}
+
+void js_document(js_serialiser_t *s)
+{
+    js_document_init(s);
+    js_document_print(s);
+}
+
+void js_document4(js_serialiser_t *s, 
+                  print_function_t print_function, 
+                  void *call_arg,
+                  int pretty_print)
+{
+    js_document_init(s);
+
     s->print = print_function;
     s->call_arg = call_arg;
+    s->pretty_print = pretty_print;
+
+    js_document_print(s);
 }
 
 void js_document_end(js_serialiser_t *s)
@@ -66,9 +112,14 @@ void js_object(js_serialiser_t *s, char *name)
     link_to_previous(s);
     indent(s);
  
-    JS_FORMAT_BUFFER("\"%s\": {\n", name);
-    s->print(s->call_arg, print_buffer);
-
+    if (s->in_array) {
+        s->print(s->call_arg, "{\n");
+    }
+    else {
+        JS_FORMAT_BUFFER("\"%s\": {\n", name);
+        s->print(s->call_arg, print_buffer);
+    }
+ 
     s->attribute_count = 0;
     s->level++;
 }
@@ -80,7 +131,31 @@ void js_object_end(js_serialiser_t *s)
     s->level--;
     indent(s);
     s->print(s->call_arg, "}");
+}
 
+void js_array(js_serialiser_t *s, char *name)
+{
+    link_to_previous(s);
+    indent(s);
+ 
+    if (s->in_array) {
+        s->print(s->call_arg, "[\n");
+    }
+    else {
+        JS_FORMAT_BUFFER("\"%s\": [\n", name);
+        s->print(s->call_arg, print_buffer);
+    }
+    s->in_array = 1; 
+    s->attribute_count = 0;
+    s->level++;
+}
+
+void js_array_end(js_serialiser_t *s)
+{
+    s->level--;
+    indent(s);
+    s->print(s->call_arg, "]");
+    s->in_array = 0;
 }
 
 void js_number(struct js_serialiser *s, char *name, double value)
@@ -88,8 +163,14 @@ void js_number(struct js_serialiser *s, char *name, double value)
     link_to_previous(s);
     indent(s);
 
-    JS_FORMAT_BUFFER("\"%s\": %1.1f", name, value);
-    s->print(s->call_arg, print_buffer);
+    if (s->in_array) {
+        JS_FORMAT_BUFFER("%1.1f", value);
+        s->print(s->call_arg, print_buffer);
+    }
+    else {
+        JS_FORMAT_BUFFER("\"%s\": %1.1f", name, value);
+        s->print(s->call_arg, print_buffer);
+     }
 }
 
 void js_int_number(struct js_serialiser *s, char *name, long value)
@@ -97,8 +178,14 @@ void js_int_number(struct js_serialiser *s, char *name, long value)
     link_to_previous(s);
     indent(s);
 
-    JS_FORMAT_BUFFER("\"%s\": %ld", name, value);
-    s->print(s->call_arg, print_buffer);
+    if (s->in_array) {
+        JS_FORMAT_BUFFER("%ld", value);
+        s->print(s->call_arg, print_buffer);
+    }
+    else {
+        JS_FORMAT_BUFFER("\"%s\": %ld", name, value);
+        s->print(s->call_arg, print_buffer);
+    }
 }
 
 void js_string(js_serialiser_t *s, char *name, char *value)
@@ -106,8 +193,14 @@ void js_string(js_serialiser_t *s, char *name, char *value)
     link_to_previous(s);
     indent(s);
 
-    JS_FORMAT_BUFFER("\"%s\": \"%s\"", name, value);
-    s->print(s->call_arg, print_buffer);
+    if (s->in_array) {
+        JS_FORMAT_BUFFER("\"%s\"", value);
+        s->print(s->call_arg, print_buffer);
+    }
+    else {
+        JS_FORMAT_BUFFER("\"%s\": \"%s\"", name, value);
+        s->print(s->call_arg, print_buffer);
+     }
 }
 
 void js_boolean(js_serialiser_t *s, char *name, int boolean)
@@ -115,13 +208,46 @@ void js_boolean(js_serialiser_t *s, char *name, int boolean)
     link_to_previous(s);
     indent(s);
 
-    JS_FORMAT_BUFFER("\"%s\": %s", name, boolean ? "true" : "false");
-    s->print(s->call_arg, print_buffer);
+    if (s->in_array) {
+        JS_FORMAT_BUFFER("%s", boolean ? "true" : "false");
+        s->print(s->call_arg, print_buffer);
+    }
+    else {
+        JS_FORMAT_BUFFER("\"%s\": %s", name, boolean ? "true" : "false");
+        s->print(s->call_arg, print_buffer);
+    }
 }
 
+void test_array_elements_do_not_have_names()
+{
+  char buffer[JS_BUFLEN_SIZE];
+  memset(buffer, 0, sizeof(buffer));
+
+  print_buffer_t output;
+  memset(&output, 0, sizeof(output));
+  output.output = buffer;
+  output.max_length = JS_BUFLEN_SIZE;
+
+  js_serialiser_t s;
+
+  js_document4(&s, print_to_buffer, &output, 1);
+  js_array(&s, "foo");
+    js_int_number(&s, "i", 1);
+    js_number(&s, "n", 2);
+    js_boolean(&s, "bool", 1);
+    js_object(&s, "object");
+    js_object_end(&s);
+    js_string(&s, "str", "foo");
+  js_array_end(&s);
+  js_document_end(&s);
+
+  printf("output:\n%s\n", buffer);
+}
 
 int main(void)
 {
+  test_array_elements_do_not_have_names();
+/*
   js_serialiser_t s;
 
   js_document(&s);
@@ -135,11 +261,17 @@ int main(void)
       js_number(&s, "bar", 3.005);
     js_object_end(&s);
     js_boolean(&s, "true", 1);
+    js_array(&s, "array");
+      js_boolean(&s, "e1", 1);
+      js_boolean(&s, "e2", 0);
+      js_boolean(&s, "e3", 1);
+      js_boolean(&s, "e4", 0);
+    js_array_end(&s); 
     js_boolean(&s, "false", 0);
   js_document_end(&s);
 
   printf("\n");
-
+*/
   return 0;
 }
 
--- json.h
+++ json.h
@@ -1,27 +1,33 @@
 #ifndef JSON_H
 #define JSON_H
 
-typedef void (*print_function_t)(const void *call_arg, const char *output);
+typedef void (*print_function_t)(void *call_arg, const char *output);
 
 struct js_serialiser
 {
     int level;
     int attribute_count;
+    int in_array;
 
     print_function_t print; 
     void *call_arg;
+    int pretty_print;
 };
 typedef struct js_serialiser js_serialiser_t;
 
 
 void js_document(js_serialiser_t *s);
-void js_document3(js_serialiser_t *s, 
+void js_document4(js_serialiser_t *s, 
                   print_function_t print_function, 
-                  void *call_arg); 
+                  void *call_arg,
+                  int pretty_print); 
 void js_document_end(js_serialiser_t *s);
 
 void js_object(js_serialiser_t *s, char *name);
 void js_object_end(js_serialiser_t *s);
+
+void js_array(js_serialiser_t *s, char *name);
+void js_array_end(js_serialiser_t *s);
 
 void js_number(struct js_serialiser *s, char *name, double value);
 void js_int_number(struct js_serialiser *s, char *name, long value);
